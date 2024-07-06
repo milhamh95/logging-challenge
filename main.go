89e9420ba189db13380ce4fcfa5c304a8d170b56
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2024-07-06 12:22:52 +0700

    feat: log level

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/rs/zerolog/log"
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	log.Info().Msg("hello world")
+	log.Debug().Msg("asdf")
+	log.Warn().Msg("asdf")
+	log.Error().Msg("asdf")
+	log.Fatal().Msg("asdf")
 }
