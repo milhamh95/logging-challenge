0cbc368f1d44edaafb569afe0466b5bc7708b7c6
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2024-07-06 14:37:54 +0700

    feat: write log output

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"context"
+	"os"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	log := log.With().Str("booking_id", "1234").Logger()
+	lf, err := os.OpenFile(
+		"logs/app.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0666,
+	)
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to open log file")
+	}
 
+	multiwriters := zerolog.MultiLevelWriter(os.Stdout, lf)
+	log.Logger = zerolog.New(multiwriters).With().Timestamp().Logger()
+
+	log := log.With().Str("booking_id", "1234").Logger()
 	ctx := log.WithContext(context.Background())
 	makeBooking(ctx)
 
